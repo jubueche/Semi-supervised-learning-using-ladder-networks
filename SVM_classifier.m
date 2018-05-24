f51d493e816cc87af8d108f21419d616e706b4a3
jubueche <jubueche@ethz.ch>
2018-05-24 23:54:31 +0200

    added 92.8%

--- SVM_classifier.m
+++ SVM_classifier.m
@@ -1,17 +1,21 @@
 %% Load data
 data = table2array(importfile('train_labeledCSV.csv'));
+data_unlabeled = table2array(importfile('train_unlabeledCSV.csv'));
 test_data = table2array(importfile_test_data('testCSV.csv'));
 X = data(:,2:129);
 y = data(:,1);
 %% Train the classifier
 predictors = X;
 response = y;
+b_c = linspace(0.001,10,10);
+for i=1:10
 
+rng(1)
 template = templateSVM(...
     'KernelFunction', 'polynomial', ...
-    'PolynomialOrder', 3, ...
+    'PolynomialOrder', 4, ...
     'KernelScale', 'auto', ...
-    'BoxConstraint', 1, ...
+    'BoxConstraint', b_c(i), ...
     'Standardize', true);
 classificationSVM = fitcecoc(...
     predictors, ...
@@ -33,5 +37,23 @@ partitionedModel = crossval(trainedClassifier.ClassificationSVM, 'KFold', 5);
 [validationPredictions, validationScores] = kfoldPredict(partitionedModel);
 
 % Compute validation accuracy
-validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
+b_c(i)
+validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError')
+end
+%% Write to csv
+predicted_test_data = trainedClassifier.predictFcn(test_data);
 
+ready_csv = zeros(8000,2);
+ready_csv(:,1) = linspace(30000,37999,8000);
+ready_csv(:,2) = predicted_test_data;
+
+csvwrite('sol.csv',ready_csv);
+
+%% Predict unlabled data
+predicted_unlabeled = trainedClassifier.predictFcn(data_unlabeled(:,1:128));
+extended = zeros(30000,129);
+extended(1:9000,:) = data;
+extended(9001:end,1) = predicted_unlabeled;
+extended(9001:end,2:129) = data_unlabeled(:,1:128);
+
+csvwrite('extended_data.csv',extended);
